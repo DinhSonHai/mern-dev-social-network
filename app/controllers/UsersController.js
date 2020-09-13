6faafbe8174b648a267e0381f7bbcf396a949442
DinhSonHai <sdinhhai@gmail.com>
2020-09-13 11:37:41 +0700

    Create register feature

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -1,4 +1,8 @@
 const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
 
 class UsersController {
   // [GET] /api/users
@@ -7,13 +11,51 @@ class UsersController {
   }
 
   //[POST] /api/users
-  create(req, res, next) {
+  async create(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('Users API');
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200', //size
+        r: 'pg', //rating
+        d: 'mm', //default
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+
+      res.send('User Register');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 }
 
