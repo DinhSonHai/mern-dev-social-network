468c2456611d9b3738ce0e4350b28cf23bd26b02
DinhSonHai <sdinhhai@gmail.com>
2020-09-14 09:50:57 +0700

    Create verity token middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../app/middlewares/auth');
 
 const AuthController = require('../../app/controllers/AuthController');
 
-router.get('/', AuthController.index);
+router.get('/', auth, AuthController.index);
 
 module.exports = router;
