7f6115103bc475753ce206021c72d663a3a25d3c
DinhSonHai <sdinhhai@gmail.com>
2020-09-16 14:43:30 +0700

    Add post route

--- app/controllers/PostsController.js
+++ app/controllers/PostsController.js
@@ -1,7 +1,26 @@
+const { check, validationResult } = require('express-validator');
+const Post = require('../models/Post');
+const Profile = require('../models/Profile');
+const User = require('../models/User');
+
 class PostsController {
-  // [GET] /api/posts
+  // @route   GET api/posts
+  // @desc    Get all post
+  // @access  Public
   index(req, res, next) {
     res.send('Posts API');
+  }
+
+  // @route   POST api/posts
+  // @desc    Create a post
+  // @access  Private
+  async create(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User.findById(req.user.id);
   }
 }
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
+const auth = require('../../app/middlewares/auth');
 const PostsController = require('../../app/controllers/PostsController');
 
+// @route   GET api/posts
+// @desc    Get all post
+// @access  Public
 router.get('/', PostsController.index);
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  PostsController.create
+);
 
 module.exports = router;
