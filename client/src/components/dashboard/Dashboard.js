24317b63d5b71f3a57d028bc5b59bd3349979930
DinhSonHai <sdinhhai@gmail.com>
2020-09-19 14:42:50 +0700

    Create private route

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = () => {
+  return <div className="dashboard">Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { loading, isAuthenticated },
+  ...rest
+}) => {
+  return (
+    <div className="privateRoute">
+      <Route
+        {...rest}
+        render={(props) =>
+          !isAuthenticated && !loading ? (
+            <Redirect to="/login" />
+          ) : (
+            <Component {...props} />
+          )
+        }
+      />
+    </div>
+  );
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
