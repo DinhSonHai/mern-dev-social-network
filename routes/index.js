3afa735dcf5b267141fb2f607c993555b34fc70a
DinhSonHai <sdinhhai@gmail.com>
2020-09-23 17:00:57 +0700

    Fix some bug

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ function route(app) {
   app.use('/api/profile', profile);
   app.use('/api/posts', posts);
   app.use('/', (req, res) => res.send('API running'));
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 module.exports = route;
